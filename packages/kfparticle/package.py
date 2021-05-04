cc07e5076504aac5522274a6f4fce8363e709735
Dmitri Smirnov <dmixsmi@gmail.com>
2021-05-03 22:11:19 -0400

    [pkg] kfparticle: Add new package

---
+++ packages/kfparticle/package.py
@@ -0,0 +1,10 @@
+from spack import *
+
+
+class Kfparticle(CMakePackage):
+
+    url = "https://git.cbm.gsi.de/m.zyzak/KFParticle/-/archive/v1.1/KFParticle-v1.1.tar.gz"
+
+    version('1.1', sha256='f06005ec67df35e3c64a377a4e720dc73d7c487519dbd1d96ff98e3a7d22f2ed')
+
+    depends_on('root')
