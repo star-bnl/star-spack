9d821eef344859435d139b618ab8110fc575facc
Dmitri Smirnov <dmixsmi@gmail.com>
2022-06-02 16:41:39 -0400

    Bump version of star-env to 0.1.6

--- packages/star-env/package.py
+++ packages/star-env/package.py
@@ -6,7 +6,7 @@ class StarEnv(BundlePackage):
 
     homepage = "https://github.com/star-bnl/star-spack/"
 
-    version('0.1.5')
+    version('0.1.6')
 
     depends_on('boost')
     depends_on('eigen')
